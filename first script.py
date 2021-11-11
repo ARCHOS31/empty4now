fa0a16d79efbd87851965cc66c2a831acd51c09b
ARCHOS31 <69053475+ARCHOS31@users.noreply.github.com>
2021-11-11 16:01:45 +0700

    Add files via upload

---
+++ first script.py
@@ -0,0 +1,21 @@
+#приветствие
+
+print("Hi")
+print(" ")
+#загрузка
+print("loading")
+print("0%")
+import time
+time.sleep (0.6)
+print("100%")
+#код
+variable1 = input("set variable1: ")
+print(" ")
+variable2 = input("set variable 2: ")
+#результат
+print("переменная 1 равна " + variable1)
+print("  ")
+print("переменная 2 равна " + variable2)
+print(" ")
+print("весь результат: " + variable1 + variable2 + " и это круто")
+print(1937 + 83637)
